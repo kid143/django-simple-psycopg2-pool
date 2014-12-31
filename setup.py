2c5b44886343e91f2691cea7a41d60cb92209f23
kid143 <primecriminalkid@gmail.com>
2014-12-31 11:39:24 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,31 @@
 
 from distutils.core import setup
 
+required = [
+    'psycopg2',
+    'Django'
+]
+
 setup(
     name='django-simple-psycopg2-pool',
     version='1.0.0',
     packages=['psycopg2_simple_pool'],
-    url='',
+    url='http://182.92.227.154/kid143/django-simple-psycopg2-pool',
     license='MIT',
     author='kid143',
     author_email='h@anrenmind.com',
-    description='A simple psycopg2 connection pool for Django'
+    description='A simple psycopg2 connection pool for Django',
+    long_description= open('README.md', 'r').read(),
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
 )
